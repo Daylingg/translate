542ec803400b57e8469244ab5da23ea6e0bac8fe
Daylin <daygarciag@gmail.com>
2024-04-04 11:50:23 -0400

    first commit

--- src/helper/helpHttp.ts
+++ src/helper/helpHttp.ts
@@ -1,21 +1,19 @@
-
 export async function postData(url: string, data: any) {
-    const token = "gAAAAABl07x3O_FuE-L8vKFgoMKun1R_SsNryMZWgTpFQ7joZiqZr5xTGWI-rVpcG-z5Z82C5NWxmddQpInvwCTbtAV2hRQDimOIeGEIZxs9BZKiRHSThuwiv99h6ehNmWsLtw9siHpd"
+  const token = import.meta.env.TOKEN
 
-    
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `token ${token}` 
-      },
-      body: JSON.stringify(data),
-    });
-    
-    if (!response.ok) {
-      throw new Error('Error en la solicitud POST');
-    }
-    
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `token ${token}`,
+    },
+    body: JSON.stringify(data),
+  })
+
+  if (!response.ok) {
+    throw new Error('Error en la solicitud POST')
   }
+
+  const responseData = await response.json()
+  return responseData
+}
