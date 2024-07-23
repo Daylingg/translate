c7af7d4e451778e0f6861ca56e68b4d35a322e6c
Daylin <daygarciag@gmail.com>
2024-07-23 17:00:47 -0400

    increment languages

--- src/constants.ts
+++ src/constants.ts
@@ -2,18 +2,28 @@ interface LanguageMap {
   [key: string]: string;
 }
 //tipos de lenguages q va a soportar
-export const SUPPORT_LANGUAGES: LanguageMap ={
-  'es':'Español',
-  'en':'English',
-  'de':'Deutsch'
+export const SUPPORT_LANGUAGES: LanguageMap = {
+  'es': 'Español',
+  'en': 'English',
+  'de': 'Deutsch',
+  "fr": 'French',
+  "it": 'Italian',
+  "pt": 'Portuguese',
+  "ru": 'Russian',
+  "zh": 'Chinese'
 }
 
 //de este sitio saco las referencias del audio https://www.techonthenet.com/js/language_tags.php
 export const VOICE_FOR_LANGUAGE = {
   en: 'en-GB',
   es: 'es-MX',
-  de: 'de-DE'
+  de: 'de-DE',
+  fr: 'fr-FR',
+  it: 'it-IT',
+  pt: 'pt-PT',
+  ru: 'ru-RU',
+  zh: 'zh-CN'
 }
 
 //lenguage automatico
-export const AUTO_LANGUAGE='auto'
+export const AUTO_LANGUAGE = 'auto'
